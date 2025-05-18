a97c8b0ff16be60b449d147b027ebe8d1cfcd856
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2025-05-18 23:09:46 +0600

    Feat:bcrypt has been implemented

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hrdcqgm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,6 +26,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db("EventManagement").collection("users");
+
+    //user data save
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const hashedPassword = await bcrypt.hash(user.password, saltRounds);
+      const newUser = {
+        name: user.name,
+        email: user.email,
+        password: hashedPassword,
+        photo: user.photo,
+      };
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -31,7 +50,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
